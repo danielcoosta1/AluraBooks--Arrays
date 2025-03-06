cad6447265c7ed156fc86aaff91f806648ad8fac
Daniel Costa <daniel.dc697@gmail.com>
2025-03-06 09:00:37 -0300

    retirando o exemplo comentado(estudo de sort)

--- app/ordenarPreco.js
+++ app/ordenarPreco.js
@@ -1,21 +1,3 @@
-// let precos = [1,3,2,5,4,8,7,600];
-// console.log(precos)
-
-
-// // function compareNumbers(a, b) {
-// //     return a - b;
-// //   }
-
-// // let precosOrdenados = precos.sort(compareNumbers);
-
-// let precosOrdenados = precos.sort((a,b)=>{
-//     return a-b;
-// });
-
-
-
-// console.log(precosOrdenados);
-
 
 const ordenarPrecosBtn = document.getElementById("btnOrdenarPorPreco");
 
