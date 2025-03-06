d18e00627174add058696045b7dabf452d783289
Daniel Costa <daniel.dc697@gmail.com>
2025-03-06 08:38:21 -0300

    alterando a função filtrar(outra semantica)

--- app/filtrarLivros.js
+++ app/filtrarLivros.js
@@ -1,10 +1,15 @@
 
 const allBtnLivros = document.querySelectorAll(".btn");
-allBtnLivros.forEach(btn => btn.addEventListener("click",filtrarLivros));
-
-function filtrarLivros(){
-    const elementoBtn = document.getElementById(this.id); //elemento que disparou o evento
+allBtnLivros.forEach(btn => btn.addEventListener("click",(evento)=>{
+    const elementoBtn = document.getElementById(evento.target.id); //elemento que disparou o evento
     const categoria = elementoBtn.value
     let livrosFiltrados = livros.filter(livro=> livro.categoria == categoria);
     exibirLivros(livrosFiltrados);
-}
+}));
+
+// function filtrarLivros(){
+//     const elementoBtn = document.getElementById(this.id); //elemento que disparou o evento
+//     const categoria = elementoBtn.value
+//     let livrosFiltrados = livros.filter(livro=> livro.categoria == categoria);
+//     exibirLivros(livrosFiltrados);
+// }
